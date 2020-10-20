bd60cc69fe5f80ef6544d394ab7e08bddc91f247
Adam Hauze <hauze_v@protonmail.com>
2020-10-20 10:17:53 -0400

    Added questions and intialized Quiz object inside my IIFE.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
 import Question from "./Question.js";
 import Quiz from "./Quiz.js";
 
-const q1 = new Question("How old is Adam?", [25, 26, 27, 28], 1);
-const q2 = new Question("Which of the following is NOT a hobby of his?", ["Running", "Gaming", "Snowboarding", "Reading"], 0);
-const q3 = new Question("What sport did he play most growing up?", ["Baseball", "Football", "Track & Field", "Basketball"], 3);
-const q4 = new Question("What generation is he in his family lineage?", ["Jr.", "III", "IV", "V"], 3);
-const q5 = new Question("What is the profession of his soon-to-be wife?", ["Accountant", "Nurse", "Marketing Director", "Yoga Instructor"], 1);
-
-const qArray = [q1, q2, q3, q4, q5];
-
 /* We use an IIFE (Immediately Invoked Function Expression) to run the app */
 const App = (() => {
   /* Cache the DOM */
@@ -20,5 +12,14 @@ const App = (() => {
   const choicesEl = document.querySelector(".quiz__choices");
   const restartButtonEl = document.querySelector(".restart");
   const nextButtonEl = document.querySelector(".next");
-})();
 
+  /* Create the question objects and add them to the quiz initialization */
+  const q1 = new Question("How old is Adam?", [25, 26, 27, 28], 1);
+  const q2 = new Question("Which of the following is NOT a hobby of his?", ["Running", "Gaming", "Snowboarding", "Reading"], 0);
+  const q3 = new Question("What sport did he play most growing up?", ["Baseball", "Football", "Track & Field", "Basketball"], 3);
+  const q4 = new Question("What generation is he in his family lineage?", ["Jr.", "III", "IV", "V"], 3);
+  const q5 = new Question("What is the profession of his soon-to-be wife?", ["Accountant", "Nurse", "Marketing Director", "Yoga Instructor"], 1);
+
+  /* Initialize Quiz object */
+  const myQuiz = new Quiz([q1, q2, q3, q4, q5]);
+})();
